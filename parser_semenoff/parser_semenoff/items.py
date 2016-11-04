54ca03234ff8ff7f88f451dca0c1ec90595fea83
sasha <sasha.burmey@gmail.com>
2016-11-04 12:55:17 +0200

    create project

---
+++ parser_semenoff/parser_semenoff/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class RosreestrItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    address = scrapy.Field()
+    lon = scrapy.Field()
+    lan =scrapy.Field()
+    pass
---
+++ parser_semenoff/parser_semenoff/spiders/rosreestr.py
@@ -0,0 +1,25 @@
+# coding: utf8
+import scrapy
+import json
+import csv
+import sys
+from scrapy import Selector
+from parser_semenoff.items import RosreestrItem
+class AsosSpider(scrapy.Spider):
+    name = "rosreestr"
+
+    def start_requests(self):
+        url = 'http://pkk5.rosreestr.ru/api/features/1?text=%D0%92%20%D0%B3%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0%D1%85%20%D1%80%D0%B0%D0%B9%D0%BE%D0%BD%D0%B0%2061:44&tolerance=10000&limit=100000&sqo=61:44&sqot=3'
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        print response.body
+        adress = RosreestrItem()
+        json_reestr = json.loads(response.body)
+        for item in json_reestr['features']:
+            if item['attrs']['address']:
+                str = item['attrs']['address']
+                adress['address'] = str.encode('utf-8')
+                adress['lat'] = item['center']['x']
+                adress['lon'] = item['center']['y']
+                yield adress
